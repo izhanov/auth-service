0dd8e96344778b8e9ad1834f00bab1460b13c5b5
Izhanov <ibekable@gmail.com>
2020-06-07 21:03:50 +0600

    [Feature]Added before_create callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@
 
 class User < Sequel::Model
   one_to_many :sessions, class: :UserSession
+
+  def before_create
+    self.email = email.downcase.strip
+    self.password_digest = BCrypt::Password.create(password_digest.strip)
+    super
+  end
+
+  def password_valid?(password)
+    BCrypt::Password.new(password_digest) == password
+  end
 end
