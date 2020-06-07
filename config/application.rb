28ee8797ad1bb49dde8a6170bf968ee1ece9a1da
Izhanov <ibekable@gmail.com>
2020-06-07 21:08:07 +0600

    [Feature]Configured base application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Application < Roda
+  opts[:root] = File.expand_path("..", __dir__)
+
   plugin :json
   plugin :all_verbs
+  plugin :param_matchers
+
+  include Dry::Monads[:result]
 end
