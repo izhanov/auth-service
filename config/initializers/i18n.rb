8b86952d3dd817e3412e882a9c9b37623d9afb02
Izhanov <ibekable@gmail.com>
2020-06-07 21:24:52 +0600

    Changed root path insert

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,3 @@
-root = File.expand_path("../..", __dir__)
-
-I18n.load_path += Dir[File.join(root, '/config/locales/**/*.yml')]
+I18n.load_path += Dir["#{Application.opts[:root]}/config/locales/**/*.yml"]
 I18n.available_locales = %i[en ru]
 I18n.default_locale = :ru
