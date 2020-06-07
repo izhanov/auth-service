2fc371926a44cb71d1b124aaba935161a1e53e7d
Izhanov <ibekable@gmail.com>
2020-06-07 21:28:51 +0600

    Configured application_helper

--- spec/application_helper.rb
+++ spec/application_helper.rb
@@ -7,9 +7,9 @@ ENV["RACK_ENV"] ||= "test"
 require_relative "../config/environment"
 
 abort("You run tests in production mode. Please don\'t do this!") if ENV["RACK_ENV"] == "production"
-root = File.expand_path("..", __dir__)
-Dir[File.join(root, "spec/support/**/*.rb")].sort.each { |file| require file }
+Dir[File.join(Application.opts[:root], "spec/support/**/*.rb")].sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+  config.include Features::RouteHelpers
 end
