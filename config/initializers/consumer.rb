6c6c765c87a8c9d4f501945f06d2376fae112693
i60 <ibekable@gmail.com>
2020-08-10 22:28:15 +0600

    [Feature]Added RPC auth

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -5,6 +5,7 @@ queue = channel.queue("authentication", durable: true)
 
 queue.subscribe do |deleviry_info, properties, payload|
   token = JSON.parse(payload).dig("token")
+  puts token
   operation = Operations::UserSessions::Authenticate.new
   session = operation.call(token)
 
@@ -16,7 +17,7 @@ queue.subscribe do |deleviry_info, properties, payload|
     )
   else
     exchange.publish(
-      {error: I18n.t("sessions.token.invalid")}.to_json,
+      { error: I18n.t("sessions.token.invalid") }.to_json,
       routing_key: properties.reply_to,
       correlation_id: properties.correlation_id
     )
